abebc9725dd87903328cfead0d40dd74c1fb303c
Carlos Vinicius <cv5673@gmail.com>
2022-08-29 16:48:47 -0300

    Initial Commit

---
+++ converter/converterApp.swift
@@ -0,0 +1,17 @@
+//
+//  converterApp.swift
+//  converter
+//
+//  Created by Carlos Vinicius on 29/08/22.
+//
+
+import SwiftUI
+
+@main
+struct converterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
